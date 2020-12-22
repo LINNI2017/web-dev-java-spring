3fa12d0ea4a988545ee89bebb94ddb3609c2799d
LINNI2017 <cai20170607@gmail.com>
2020-12-22 00:38:06 -0800

    add simple web app

---
+++ wk1-web-app/src/test/java/com/example/wk1webapp/Wk1WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wk1webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk1WebAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
